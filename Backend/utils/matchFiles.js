5a79719abac3fd3c8704be5732e62d74879e275b
João Pedro Sá <113131816+joaopssa@users.noreply.github.com>
2025-11-24 21:32:33 -0300

    preparando o terreno

---
+++ Backend/utils/matchFiles.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  "Brasileirão Série A": path.join(__dirname, "../data/matches/brasileirao-serie-a/2025_br1.txt"),
+  "Brasileirão Série B": path.join(__dirname, "../data/matches/brasileirao-serie-b/2025_br2.txt"),
+  "Bundesliga": path.join(__dirname, "../data/matches/bundesliga/2025_26_bundesliga.txt"),
+  "Liga dos Campeões": path.join(__dirname, "../data/matches/champions/2025_26_cl.txt"),
+  "La Liga": path.join(__dirname, "../data/matches/laliga/2025_26_liga.txt"),
+  "Copa Libertadores": path.join(__dirname, "../data/matches/libertadores/2025_copal.txt"),
+  "Ligue 1": path.join(__dirname, "../data/matches/ligue1/2025-26_fr1.txt"),
+  "Premier League": path.join(__dirname, "../data/matches/premier-league/2025_26_premierleague.txt"),
+  "Série A": path.join(__dirname, "../data/matches/serie-a-tim/2025_26_seriea.txt"),
+};
